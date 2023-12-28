e9fce3d0b033dd8210744cfdfe2b04810786cfac
mrodriguezbe <mauricio_rodriguezbertella@mercadolibre.com>
2023-12-28 13:26:11 -0300

    ejercicio3

---
+++ Clase1/Practica1/ejercicio3/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("jajas")
+	/*
+		var 1firstName string INCORRECTA var firstName string
+
+		var lastName string CORRECTA
+
+		var int age CORRECTA
+
+		1lastName := 6 INCORRECTA lastName := 6
+
+		var driver_license = true INCORRECTA var driverLicense = true
+
+		var person height int INCORRECTA var personHeight int
+
+		childsNumber := 2  CORRECTA*/
+}
