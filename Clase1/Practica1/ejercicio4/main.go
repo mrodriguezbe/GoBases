2844d9a4f97dbe76525a0f3cd85e41efea3828a9
mrodriguezbe <mauricio_rodriguezbertella@mercadolibre.com>
2023-12-28 13:29:15 -0300

    ejercicio 4

---
+++ Clase1/Practica1/ejercicio4/main.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+	print("main")
+	/*
+
+		var lastName string = "Smith" OK
+		var age int = "35" INCORRECTO, es sin las comillas
+
+		boolean := "false" INCORRECTO es sin las comillas
+
+		var salary string = 45857.90 INCORRECTO deberia ser un float
+
+		var firstName string = "Mary" CORRECTO
+	*/
+}
